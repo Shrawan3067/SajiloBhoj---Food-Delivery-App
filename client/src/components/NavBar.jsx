5bdb593769df30e4c2cedc7630c5a773b3d4af2f
Shrawan <shrawansah3067@gmail.com>
2025-09-24 12:47:03 +0530

    Nothing changed

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -14,6 +14,7 @@ import {
 import { FiSearch } from "react-icons/fi";
 import { BiSolidOffer } from "react-icons/bi";
 import { CartContext } from "../context/CartContext";
+import '../index.css';
 
 export default function NavBar() {
   const [isLoggedIn] = useState(true);
