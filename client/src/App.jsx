915ded8653d1ec8b129882f8599a11373eaf6cef
Shrawan <shrawansah3067@gmail.com>
2025-09-26 23:39:48 +0530

    Nothing changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,7 +25,7 @@ import OrderHistory from './pages/OrderHistory';
 import OrderTracking from './pages/OrderTracking';
 import OrderConfirmation from './pages/OrderConfirmation';
 import Loader from './components/Loader'; // Import the loader
-import './index.css';
+import './App.css';
 
 function Layout() {
   const location = useLocation();
